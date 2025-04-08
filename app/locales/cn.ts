cfe538fe55b90aa6830c478f17ef75eba43acc32
Kuenpan Foo <61587972+fqbin@users.noreply.github.com>
2025-04-08 18:19:09 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -7,7 +7,8 @@ const isApp = !!getClientConfig()?.isApp;
 const cn = {
    WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: `点击[这里](/#/auth)输入访问秘钥 🔑`,
+    Unauthorized: `我们的应用目前不对外提供服务，敬请谅解。
+                \\ 点击[这里](/#/auth)输入访问秘钥 🔑`,
   },
   Auth: {
     Return: "返回",
@@ -17,9 +18,8 @@ const cn = {
     Input: "在此处填写访问码",
     Confirm: "确认",
     Later: "稍后再说",
-    SaasTips: "我们的应用目前不对外提供服务，敬请谅解。",
-    TopTips:
-      "",
+    SaasTips: "",
+    TopTips: "",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
@@ -618,7 +618,7 @@ const cn = {
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
       Topic:
-        "使用四到五个字直接返回这句话的简要主题，不要解释、不要标点、不要语气词、不要多余文本，不要加粗，如果没有主题，请直接返回“闲聊”",
+        "使用四到五个字直接返回这句话的简要主题，不要解释、不要标点、不要语气词、不要多余文本，不要加粗，如果没有主题，请直接返回"闲聊"",
       Summarize:
         "简要总结一下对话内容，用作后续的上下文提示 prompt，控制在 200 字以内",
     },
